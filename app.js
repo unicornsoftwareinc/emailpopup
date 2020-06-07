c4afc884e6882e61fbfc09265602d4f859f1a62a
Philip Schaffer <phil917@gmail.com>
2020-06-07 16:34:16 -0400

    added dashboard

--- app.js
+++ app.js
@@ -145,6 +145,7 @@ app.post('/account/profile', passportConfig.isAuthenticated, userController.post
 app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
+app.get('/dashboard', passportConfig.isAuthenticated, userController.getDashboard);
 
 /**
  * API examples routes.
