b0623c51db9eb4e6ba7b42cda5b7070489fa3815
Philip Schaffer <phil917@gmail.com>
2020-07-04 23:50:12 -0400

    email pop up js code

--- public/js/emailPopUp.js
+++ public/js/emailPopUp.js
@@ -2,14 +2,10 @@ let background = document.getElementById('emailPopUpExampleBackground');
 
 background.style.display = 'block';
 
-background.onclick = function() {
+background.onclick = function(event) {
 	background.style.display = 'none';
 }
 
 document.getElementById('emailPopUpExampleClose').onclick = function() {
 	background.style.display = 'none';
-}
-
-document.getElementById('emailPopUpExampleForm').onclick = function() {
-	return false;
 }
